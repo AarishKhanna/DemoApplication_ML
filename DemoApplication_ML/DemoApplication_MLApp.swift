a37cec43959117a46c9025cfeb0504771b19650b
Aarish Khanna <68593746+AarishKhanna@users.noreply.github.com>
2023-04-24 14:13:07 +0530

    Initial Commit

---
+++ DemoApplication_ML/DemoApplication_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApplication_MLApp.swift
+//  DemoApplication_ML
+//
+//  Created by Aarish Khanna on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApplication_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoApplication_MLUITests/DemoApplication_MLUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DemoApplication_MLUITestsLaunchTests.swift
+//  DemoApplication_MLUITests
+//
+//  Created by Aarish Khanna on 24/04/23.
+//
+
+import XCTest
+
+final class DemoApplication_MLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
